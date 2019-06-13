72e8e71e06958bc9c748963edbf87cb4b1d5a4b3
Victor Saa <victorsaagbmediagroup@gamil.com>
2019-06-13 18:49:47 -0500

    enviroment ready

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+	name: 'app',
+	components: {}
+}
+</script>
