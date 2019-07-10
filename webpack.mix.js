a9753efac37505e110506e73e470fc8fe127b235
Victor Saa <victorsaagbmediagroup@gamil.com>
2019-07-10 16:03:41 -0500

    font fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -36,14 +36,22 @@ mix.js('src/app.js', 'public_html/js').sass('src/scss/main.scss', 'public_html/c
 				]
 			},
 			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [{
-					loader: 'file-loader',
+				// Match woff2 in addition to patterns like .woff?v=1.1.1.
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: {
+					loader: "url-loader",
 					options: {
-					name: 'fonts/[name].[ext]',
-						// outputPath: 'fonts/'
+						// Limit at 50k. Above that it emits separate files
+						limit: 50000,
+
+						// url-loader sets mimetype if it's passed.
+						// Without this it derives it from the file extension
+						mimetype: "application/font-woff",
+
+						// Output below fonts directory
+						name: "fonts/[name].[ext]",
 					}
-				}]
+				},
 			},
 		]
 	}
