56d1702b24f21298ae0bccf39a5fb3bab48729b5
Victor Saa <victorsaagbmediagroup@gamil.com>
2019-09-05 21:38:42 -0500

    miscelaneous

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,7 +1,9 @@
 <template>
 	<div id="appex">
 		<img alt="Vue logo" src="./../assets/logo.png">
-		<router-view></router-view>
+		<transition name="fade">
+			<router-view></router-view>
+		</transition>
 	</div>
 </template>
 
